dca07847c7130061d532b20f3e1491923614c592
Mhd-i <mhdibuis@gmail.com>
2025-03-29 14:20:09 +0100

    content selection buttons now switch changes the displayed component

---
+++ sawarni/frontend/src/app/courses-view/courses-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-view',
+  imports: [],
+  templateUrl: './courses-view.component.html',
+  styleUrl: './courses-view.component.css'
+})
+export class CoursesViewComponent {
+
+}
